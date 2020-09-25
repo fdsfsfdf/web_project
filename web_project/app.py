e8e9b926d0742830558ba3828474808af5259d27
fdsfsfdf <32867503+fdsfsfdf@users.noreply.github.com>
2020-09-25 10:39:45 +0900

    Add files via upload

--- web_project/app.py
+++ web_project/app.py
@@ -1,17 +1,18 @@
 from flask import Flask, render_template
 from bs4 import BeautifulSoup
-from selenium import webdriver
+# from selenium import webdriver
 import time
-import pandas as pd    
-import os
-import math
-import urllib.request
-import urllib
-import win32com.client as win32   #pywin32 , pypiwin32 설치후 동작
-import win32api  #파이썬 프롬프트를 관리자 권한으로 실행해야 에러없음
-import cx_Oracle
-import numpy
+# import pandas as pd    
+# import os
+# import math
+# import urllib.request
+# import urllib
+# import win32com.client as win32   #pywin32 , pypiwin32 설치후 동작
+# import win32api  #파이썬 프롬프트를 관리자 권한으로 실행해야 에러없음
+# import cx_Oracle
+# import numpy
 import requests
+
 # import pymysql
 
 def create_soup(url):
@@ -23,11 +24,14 @@ def create_soup(url):
 def naverPolitics_news(i):
     url = "https://news.naver.com/main/ranking/popularDay.nhn?rankingType=popular_day"
     soup=create_soup(url)
-    updatedNews = soup.find("li","num1").find('div','thumb').find('a')
+    updatedNews = soup.find("li",f"num{i}").find('div','thumb').find('a')
     # print(updatedNews)
     title = updatedNews['title']
+    # print(title)
     link = "https://news.naver.com" + updatedNews["href"]
+    # print(link)
     img = updatedNews.find("img")['src']
+    # print(img)
     return title,link,img
 
 app = Flask(__name__)
